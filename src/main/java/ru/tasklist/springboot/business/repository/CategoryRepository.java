e8d3c3bdb218d62bd8b1ac6dba25850e3e6af373
Aleksey Petrik <alexey.petric@gmail.com>
2022-04-10 16:46:29 +1000

    refactor.

--- src/main/java/ru/tasklist/springboot/business/repository/CategoryRepository.java
+++ src/main/java/ru/tasklist/springboot/business/repository/CategoryRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.tasklist.springboot.business.entity.Category;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    //Запрос поиска по email по имени метода
+    List<Category> findByUserEmailOrderByTitleAsc(String email);
+
 }
