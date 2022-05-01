7f0488a592ba2c719633d78fd5c16dc7d1d374af
Aleksey Petrik <alexey.petric@gmail.com>
2022-05-01 18:25:25 +1000

    refactoring, add auth filter.

--- src/main/java/ru/tasklist/springboot/auth/config/SpringConfig.java
+++ src/main/java/ru/tasklist/springboot/auth/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package ru.tasklist.springboot.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.SessionManagementFilter;
+import ru.tasklist.springboot.auth.filter.AuthTokenFilter;
 import ru.tasklist.springboot.auth.service.UserDetailsServiceImpl;
 
 @Configuration
@@ -20,9 +23,16 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
 
     private UserDetailsServiceImpl userDetailsService;
 
+    private AuthTokenFilter authTokenFilter;
+
     @Autowired
     public void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void setAuthTokenFilter(AuthTokenFilter authTokenFilter) {
+        this.authTokenFilter = authTokenFilter;
     }
 
     //Односторонний кодировщик паролей (хеширование) BCrypt
@@ -39,7 +49,15 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public FilterRegistrationBean registration(AuthTokenFilter authTokenFilter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(authTokenFilter);
+        registration.setEnabled(false);
+        return registration;
     }
 
     @Override
@@ -61,6 +79,9 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
         http.requiresChannel()
                 .anyRequest()
                 .requiresSecure(); // обязательное исп. HTTPS
+
+        http.addFilterBefore(authTokenFilter, SessionManagementFilter.class);
+
 
     }
 
