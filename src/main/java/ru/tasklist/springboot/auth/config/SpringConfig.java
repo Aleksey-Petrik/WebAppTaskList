532cea55fba9fd77769c946c0c81f9ae93f15b2c
Aleksey Petrik <alexey.petric@gmail.com>
2022-05-03 18:23:10 +1000

    refactoring, add comments.

--- src/main/java/ru/tasklist/springboot/auth/config/SpringConfig.java
+++ src/main/java/ru/tasklist/springboot/auth/config/SpringConfig.java
@@ -21,54 +21,61 @@ import ru.tasklist.springboot.auth.service.UserDetailsServiceImpl;
 // Указывает Spring контейнеру, чтобы находил файл конфигурации в классе. debug = true - для просмотра лога какие бины были созданы, в production нужно ставить false
 public class SpringConfig extends WebSecurityConfigurerAdapter {
 
+    // для получения пользователя из БД
     private UserDetailsServiceImpl userDetailsService;
 
-    private AuthTokenFilter authTokenFilter;
+    // перехватывает все выходящие запросы (проверяет jwt если необходимо, автоматически логинит пользователя)
+    private AuthTokenFilter authTokenFilter;// его нужно зарегистрировать в filterchain
 
     @Autowired
-    public void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {
+    public void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {// внедряем наш компонент Spring @Service
         this.userDetailsService = userDetailsService;
     }
 
     @Autowired
     public void setAuthTokenFilter(AuthTokenFilter authTokenFilter) {
-        this.authTokenFilter = authTokenFilter;
+        this.authTokenFilter = authTokenFilter;// внедряем фильтр
     }
 
-    //Односторонний кодировщик паролей (хеширование) BCrypt
+    // кодировщик паролей односторонним алгоритмом хеширования BCrypt https://ru.bitcoinwiki.org/wiki/Bcrypt
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // используем стандартный готовый authenticationManager из Spring контейнера (используется для проверки логина-пароля)
+    // эти методы доступны в документации Spring Security - оттуда их можно копировать, чтобы не писать вручную
     @Bean
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
 
+    // указываем наш сервис userDetailsService для проверки пользователя в БД и кодировщик паролей
+    // эти методы доступны в документации Spring Security - оттуда их можно копировать, чтобы не писать вручную
     @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {// настройки AuthenticationManager для правильной проверки логин-пароль
+        authenticationManagerBuilder.userDetailsService(userDetailsService)// использовать наш сервис для загрузки User из БД
+                .passwordEncoder(passwordEncoder());// указываем, что используется кодировщик пароля (для корректной проверки пароля)
     }
 
+    // нужно отключить вызов фильтра AuthTokenFilter для сервлет контейнера (чтобы фильтр вызывался не 2 раза, а только один раз из Spring контейнера)
+    // https://stackoverflow.com/questions/39314176/filter-invoke-twice-when-register-as-spring-bean
     @Bean
     public FilterRegistrationBean registration(AuthTokenFilter authTokenFilter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean(authTokenFilter);
-        registration.setEnabled(false);
+        FilterRegistrationBean registration = new FilterRegistrationBean(authTokenFilter);// FilterRegistrationBean - регистратор фильтров для сервлет контейнера
+        registration.setEnabled(false);// отключить исп-е фильтра для сервлет контейнера
         return registration;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // порядок следования настроек внутри метода - неважен
+        // порядок следования настроек внутри метода - неважен, можно в любой последовательности
 
         /* Если используется другая клиентская технология (не SpringMVC, а например Angular, React и пр.),
             то выключаем встроенную Spring-защиту от CSRF атак,
             иначе запросы от клиента не будут обрабатываться, т.к. Spring Security будет пытаться в каждом входящем запроcе искать спец. токен для защиты от CSRF
         */
-
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//SessionCreationPolicy.STATELESS - для того чтобы сессии не создавались
 
         http.csrf().disable(); // На время разработки проекта не будет ошибок (для POST, PUT и др. запросов) - недоступен и т.д.
@@ -80,9 +87,8 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .requiresSecure(); // обязательное исп. HTTPS
 
-        http.addFilterBefore(authTokenFilter, SessionManagementFilter.class);
-
-
+        // authTokenFilter - валидация JWT, до того, как запрос попадет в контроллер
+        http.addFilterBefore(authTokenFilter, SessionManagementFilter.class);// добавляем наш фильтр в securityfilterchain
     }
 
 }
